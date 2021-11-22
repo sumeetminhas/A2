883a1447af7a9183650bea3672bb7086480a16d3
Sumeet <sumeetm273@gmail.com>
2021-11-21 19:57:29 -0800

    renderView function

--- acit1620_a2_start-main/script.js
+++ acit1620_a2_start-main/script.js
@@ -19,7 +19,7 @@ function cleanUpIndex() {
     for (i = 0; i < removeUniqueIndex.length; i++) {
         removeUniqueIndex[i].remove()
     }
-}
+};
 
 // creates a single index card for the Index page
 function createSingleIndex(newContact) {
@@ -34,9 +34,9 @@ function createSingleIndex(newContact) {
     newDiv.appendChild(newP);
 
     newLink.appendChild(newDiv);
-    
+
     return newLink;
-}
+};
 
 //creates all of the DOM nodes that are unique to the Index page
 function renderIndex(contactList) {
@@ -47,7 +47,62 @@ function renderIndex(contactList) {
   }
 };
 
+//removes all of the DOM nodes that are unique to the View page
+function cleanUpView() {
+  let removeUniqueView = document.querySelectorAll(".contactinfo");
+  for (i = 0; i < removeUniqueView.length; i++) {
+    removeUniqueView[i].remove()
+  }
+};
 
-// function cleanUpView() {
-//   let removeUniqueView = document.querySelectorAll("")
-// }
+//creates all of the DOM nodes that are unique to the View page
+function renderView(contactList) {
+  let viewPage = document.querySelector(".main");
+
+  const contactInfo = document.createElement("div");
+  contactInfo.classList.add("contactinfo");
+  viewPage.appendChild(contactInfo);
+
+  const contactName = document.createElement("div");
+  contactName.classList.add("contactname");
+  contactName.textContent = contactList["name"];
+  contactInfo.appendChild(contactName);
+
+  const contactImg = document.createElement("img");
+  contactImg.setAttribute("src", "./img/profile.jpg");
+  contactImg.classList.add("profilepic");
+  contactImg.setAttribute("alt", "Profile picture");
+  contactName.appendChild(contactImg);
+
+  const contactEmail = document.createElement("div");
+  contactEmail.classList.add("contactemail");
+  contactEmail.textContent = `email: ${contactList["email"]}`;
+  contactInfo.appendChild(contactEmail);
+
+  const contactPhone = document.createElement("div");
+  contactPhone.classList.add("contactphone");
+  contactPhone.textContent = `cell: ${contactList["phone"]}`;
+  contactInfo.appendChild(contactPhone);
+
+  const contactAddress = document.createElement("div");
+  contactAddress.classList.add("contactaddress");
+  contactAddress.textContent = `address: ${contactList["address"]}`;
+  contactInfo.appendChild(contactAddress);
+
+  const button = document.createElement("div")
+  button.classList.add("buttons");
+  contactInfo.appendChild(button);
+
+  const editButton = document.createElement("button");
+  editButton.classList.add("button", "edit");
+  editButton.setAttribute("value", "Edit");
+  editButton.textContent = "Edit";
+  button.appendChild(editButton);
+
+  const closeButton = document.createElement("button");
+  closeButton.classList.add("button", "close");
+  closeButton.setAttribute("value", "Close");
+  closeButton.textContent = "Close";
+  button.appendChild(closeButton);
+  
+};
